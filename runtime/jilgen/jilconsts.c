325689f43716f8ff848e05fc34f36512ee268803
Nazim Uddin Bhuiyan <nazimudd@ualberta.ca>
2018-10-26 13:43:45 -0400

    further fixes

--- runtime/jilgen/jilconsts.c
+++ runtime/jilgen/jilconsts.c
@@ -75,11 +75,7 @@ createConstant(OMRPortLibrary *OMRPORTLIB, char const *name, UDATA value)
 #if defined(J9VM_ARCH_POWER) || defined(J9VM_ARCH_ARM)
 	return omrstr_printf(line, sizeof(line), "#define %s %zu\n", name, value);
 #elif defined(LINUX) /* J9VM_ARCH_POWER || J9VM_ARCH_ARM */
-	#if defined(J9VM_ENV_DATA64)
-		return omrstr_printf(line, sizeof(line), "%%define %s %zu\n", name, value);
-	#else /* J9VM_ENV_DATA64 */
-		return omrstr_printf(line, sizeof(line), "%s = %zu\n", name, value);
-	#endif
+	return omrstr_printf(line, sizeof(line), "%%define %s %zu\n", name, value);
 #elif defined(WIN32) /* LINUX */
 	return omrstr_printf(line, sizeof(line), "%s equ %zu\n", name, value);
 #elif defined(J9ZOS390) /* WIN32 */
