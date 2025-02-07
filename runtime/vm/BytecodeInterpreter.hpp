017db7ff7f03dfdf508cc018c0216c2e8007b5b7
Nazim Bhuiyan <nubhuiyan@ibm.com>
2025-02-07 14:29:05 -0500

    test6

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -9257,28 +9257,26 @@ done:
 		bool fromJIT = J9_ARE_ANY_BITS_SET(jitStackFrameFlags(REGISTER_ARGS, 0), J9_SSF_JIT_NATIVE_TRANSITION_FRAME);
 		UDATA mhReceiverIndex = 0;
 
-		PORT_ACCESS_FROM_JAVAVM(_vm);
-
 		if (fromJIT) {
 			/* tempSlot contains the number of stack slots for the arguments, and the MH
 			 * receiver is the first argument.
 			 */
-			//j9tty_printf(PORTLIB, "In invokebasic, about to compute receiver index, using tempSlot value %d\n", _currentThread->tempSlot);
-			if (_currentThread->tempSlot <= 0 || _currentThread->tempSlot > 3)
+			PORT_ACCESS_FROM_JAVAVM(_vm);
+			if (_currentThread->tempSlot <= 0 || _currentThread->tempSlot > 500){
 				j9tty_printf(PORTLIB, "In invokebasic, about to compute receiver index, using unusual tempSlot value %d\n", _currentThread->tempSlot);
+				buildJITResolveFrame(REGISTER_ARGS);
+				j9tty_printf(PORTLIB, "buildJitResolveFrame Done\n");
+				return THROW_NPE;
+			}
 			mhReceiverIndex = _currentThread->tempSlot - 1;
 		} else {
-			
 			U_16 index = *(U_16 *)(_pc + 1);
 			J9ConstantPool *ramConstantPool = J9_CP_FROM_METHOD(_literals);
 			J9RAMMethodRef *ramMethodRef = ((J9RAMMethodRef *)ramConstantPool) + index;
 			UDATA volatile methodIndexAndArgCount = ramMethodRef->methodIndexAndArgCount;
 			mhReceiverIndex = (methodIndexAndArgCount & 0xFF);
-			//j9tty_printf(PORTLIB, "In invokebasic, about to compute receiver index, using methodIndexAndArgCount\n");
 		}
 
-		if (mhReceiverIndex < 0 || mhReceiverIndex > 3)
-			j9tty_printf(PORTLIB, "mhReceiverIndex = %d\n", mhReceiverIndex);
 		j9object_t mhReceiver = ((j9object_t *)_sp)[mhReceiverIndex];
 		if (J9_UNEXPECTED(NULL == mhReceiver)) {
 			if (fromJIT) {
@@ -9296,13 +9294,9 @@ done:
 		//j9tty_printf(PORTLIB, "successfully set sendMethod\n");
 
 		if (fromJIT) {
-			if (mhReceiverIndex < 0 || mhReceiverIndex > 3)
-				j9tty_printf(PORTLIB, "restoring JIT return address\n");
 			VM_JITInterface::restoreJITReturnAddress(_currentThread, _sp, (void *)_literals);
 			rc = j2iTransition(REGISTER_ARGS, true);
 		}
-		if (mhReceiverIndex < 0 || mhReceiverIndex > 3)
-			j9tty_printf(PORTLIB, "invokebasic DONE\n");
 		return rc;
 	}
 
@@ -9415,6 +9409,23 @@ throw_npe:
 		} else {
 			methodArgCount = romMethod->argCount;
 		}
+		
+		
+		if (isInvokeBasic && (methodArgCount <= 0 || methodArgCount > 500)){
+			PORT_ACCESS_FROM_JAVAVM(_vm);
+			if (fromJIT) {
+				j9tty_printf(PORTLIB, "In invokebasic handler inlined in invokeV from JIT, unusual arg count %d\n", methodArgCount);
+				/* Restore SP to before popping memberNameObject. */
+				_sp -= 1;
+				buildJITResolveFrame(REGISTER_ARGS);
+			}
+			else {
+				j9tty_printf(PORTLIB, "In invokebasic handler inlined in invokeV, unusual arg count %d\n", methodArgCount);
+			}
+			return THROW_NPE;
+		}
+
+
 
 		j9object_t receiverObject = ((j9object_t *)_sp)[methodArgCount - 1];
 		if (J9_UNEXPECTED(NULL == receiverObject)) {
